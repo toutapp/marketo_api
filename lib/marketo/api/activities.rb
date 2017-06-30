af5d03ea8a9983f894723bb2788a1b0fb2efc2c6
Ami Bhatt <atbhatt@msn.com>
2017-06-30 12:52:29 -0700

    Specs

--- lib/marketo/api/activities.rb
+++ lib/marketo/api/activities.rb
@@ -14,6 +14,28 @@ module Marketo
       def activities_describe
         api_get('activities/types.json')
       end
+
+      def activities
+      end
+
+      def add_next_page_token(path, token)
+        if token.present?
+          path = path + "&nextPageToken=#{next_page_token}"
+        end
+        path
+      end
+
+      def activities_by_lead_ids(lead_ids, next_page_token=nil)
+        lead_ids = format_filter_values(lead_ids)
+        path = "activities.json?leadIds=#{lead_ids}"
+        api_get(add_next_page_token(path, next_page_token))
+      end
+
+      def activities_by_type_id(type_ids, next_page_token=nil)
+        type_ids = format_filter_values(type_ids)
+        path = "activities.json?activityTypeIds=#{type_ids}"
+        api_get(add_next_page_token(path, next_page_token))
+      end
     end
   end
 end
