a9a8601d3bf7f235ec86e571126b2f3b455dd896
Ami Bhatt <atbhatt@msn.com>
2017-07-05 13:55:55 -0700

    Add API example group

--- spec/unit/api/base_spec.rb
+++ spec/unit/api/base_spec.rb
@@ -1,13 +1,7 @@
 require 'spec_helper'
 
 describe Marketo::API::Base do
-  # let(:options) { double('Options') }
-
-  # before do
-  #   client.stub options: options
-  # end
-  #
-  # describe '#format_filter_values' do
-  #   expect(client.format_filter_values(['what', 'is'])).to eq('')
-  # end
+  describe '#format_filter_values' do
+    # expect(client.format_filter_values(['what', 'is'])).to eq('')
+  end
 end
