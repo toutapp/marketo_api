41be41f870d33ac0911e32d52930a3e2c35ec371
Mohnish Thallavajhula <m@mt.cx>
2017-07-31 12:11:48 -0700

    format

--- lib/marketo_api/concerns/connection.rb
+++ lib/marketo_api/concerns/connection.rb
@@ -19,34 +19,40 @@ module MarketoApi
 
       # Internal: Internal faraday connection where all requests go through
       def connection
-        @connection ||= Faraday.new(options[:instance_url],
-                                    connection_options) do |builder|
+        @connection ||= Faraday.new(options[:instance_url], connection_options) do |builder|
           # Converts the request into JSON.
-          builder.request  :json
+          builder.request(:json)
+
           # Handles reauthentication for 403 responses.
           if authentication_middleware
-            builder.use    authentication_middleware, self, options
+            builder.use(authentication_middleware, self, options)
           end
-          # Sets the oauth token in the headers.
-          builder.use      MarketoApi::Middleware::Authorization, self, options
-          # Ensures the instance url is set.
-          # builder.use      MarketoApi::Middleware::InstanceURL, self, options
-          # Caches GET requests.
-          builder.use      MarketoApi::Middleware::Caching, cache, options if cache
-          # Follows 30x redirects.
-          builder.use      FaradayMiddleware::FollowRedirects
-          # Raises errors for 40x responses.
-          builder.use      MarketoApi::Middleware::RaiseError
-          # Parses returned JSON response into a hash.
-          builder.response :json, content_type: /\bjson$/
-          # Inject custom headers into requests
-          # builder.use      MarketoApi::Middleware::CustomHeaders, self, options
-          # Log request/responses
-          builder.use      MarketoApi::Middleware::Logger,
-                           MarketoApi.configuration.logger,
-                           options if MarketoApi.log?
 
-          builder.adapter  adapter
+          # Sets the oauth token in the headers.
+          builder.use(MarketoApi::Middleware::Authorization, self, options)
+
+          # Ensures the instance url is set.
+          # builder.use(MarketoApi::Middleware::InstanceURL, self, options)
+
+          # Caches GET requests.
+          builder.use(MarketoApi::Middleware::Caching, cache, options) if cache
+
+          # Follows 30x redirects.
+          builder.use(FaradayMiddleware::FollowRedirects)
+
+          # Raises errors for 40x responses.
+          builder.use(MarketoApi::Middleware::RaiseError)
+
+          # Parses returned JSON response into a hash.
+          builder.response(:json, content_type: /\bjson$/)
+
+          # Inject custom headers into requests
+          # builder.use(MarketoApi::Middleware::CustomHeaders, self, options)
+
+          # Log request/responses
+          builder.use(MarketoApi::Middleware::Logger, MarketoApi.configuration.logger, options) if MarketoApi.log?
+
+          builder.adapter(adapter)
         end
       end
 
