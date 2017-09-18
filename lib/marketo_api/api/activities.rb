8d688feffd5adc5a486610c584f2574132f714ac
Ami Bhatt <atbhatt@msn.com>
2017-09-18 16:46:23 -0700

    Add ability to get paging token, append to activities api request path

--- lib/marketo_api/api/activities.rb
+++ lib/marketo_api/api/activities.rb
@@ -1,3 +1,5 @@
+require 'time'
+
 module MarketoApi
   module API
     module Activities
@@ -19,25 +21,38 @@ module MarketoApi
       end
 
       def paging_token(start_time)
-        api_get("activities/pagingtoken.json?sinceDateTime=#{start_time.iso8601}")
+        if start_time.present?
+          mod_time = start_time.iso8601
+        else
+          mod_time = Time.now.iso8601
+        end
+        api_get("activities/pagingtoken.json?sinceDatetime=#{mod_time}")
       end
 
       def add_next_page_token(path, token)
-        if token.present?
-          path = path + "&nextPageToken=#{next_page_token}"
-        end
+        path = path + "&nextPageToken=#{token}" if token
         path
       end
 
-      def activities_by_lead_ids(lead_ids, next_page_token=nil)
-        lead_ids = format_filter_values(lead_ids)
-        path = "activities.json?leadIds=#{lead_ids}"
+      def add_activity_type_ids(type_ids)
+        ids = format_filter_values(type_ids)
+        if ids.present?
+          "activityTypeIds=#{ids}"
+        else
+          ""
+        end
+      end
+
+      def activities_by_lead_ids(lead_ids, type_ids, next_page_token=nil)
+        path = "activities.json"
+        path << "?leadIds=#{format_filter_values(lead_ids)}" if lead_ids.present?
+        path << "&#{add_activity_type_ids(type_ids)}" if type_ids.present?
         api_get(add_next_page_token(path, next_page_token))
       end
 
       def activities_by_type_id(type_ids, next_page_token=nil)
-        type_ids = format_filter_values(type_ids)
-        path = "activities.json?activityTypeIds=#{type_ids}"
+        path = "activities.json"
+        path << "?#{add_activity_type_ids(type_ids)}" if type_ids.present?
         api_get(add_next_page_token(path, next_page_token))
       end
 
