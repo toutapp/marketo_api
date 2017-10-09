c23e09ff805d1bce8c812657cfc8e6877b82b319
Mohnish Thallavajhula <m@mt.cx>
2017-10-09 14:46:11 -0700

    lose focus

--- spec/marketo_api/api/base_spec.rb
+++ spec/marketo_api/api/base_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 describe MarketoApi::API::Base do
   subject { Class.new.extend(MarketoApi::API::Base) }
 
-  describe '#format_filter_values', :focus => true do
+  describe '#format_filter_values' do
     context 'when filter_values is a hash' do
       it 'formats the filter_values' do
         expect(subject.format_filter_values({ foo: 'bar' })).to eql({ foo: 'bar' })
