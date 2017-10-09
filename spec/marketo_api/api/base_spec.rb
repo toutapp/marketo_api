c58aab4f067255fba1560c71fd61d280a7975643
Mohnish Thallavajhula <m@mt.cx>
2017-10-09 14:44:11 -0700

    add specs for api/base

--- spec/marketo_api/api/base_spec.rb
+++ spec/marketo_api/api/base_spec.rb
@@ -1,7 +1,19 @@
 require 'spec_helper'
 
 describe MarketoApi::API::Base do
-  describe '#format_filter_values' do
-    # expect(client.format_filter_values(['what', 'is'])).to eq('')
+  subject { Class.new.extend(MarketoApi::API::Base) }
+
+  describe '#format_filter_values', :focus => true do
+    context 'when filter_values is a hash' do
+      it 'formats the filter_values' do
+        expect(subject.format_filter_values({ foo: 'bar' })).to eql({ foo: 'bar' })
+      end
+    end
+
+    context 'when filter_values is an array' do
+      it 'formats the filter_values' do
+        expect(subject.format_filter_values(['foo', 'bar'])).to eql('foo,bar')
+      end
+    end
   end
 end
