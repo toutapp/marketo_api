b040a222c4f6eeabdbad225d45adee5152fab8ad
Mohnish Thallavajhula <m@mt.cx>
2017-10-09 13:52:43 -0700

    remove integration dir

--- spec/integration/abstract_client_spec.rb
+++
@@ -1,8 +0,0 @@
-require 'spec_helper'
-
-shared_examples_for MarketoApi::AbstractClient do
-end
-
-describe MarketoApi::AbstractClient do
-  it_behaves_like MarketoApi::AbstractClient
-end
--- spec/integration/data/client_spec.rb
+++
@@ -1,8 +0,0 @@
-require 'spec_helper'
-
-shared_examples_for MarketoApi::Data::Client do
-end
-
-describe MarketoApi::Data::Client do
-  it_behaves_like MarketoApi::Client
-end
