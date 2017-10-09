8db4f5adb29787042ec09f534a457fd6a54798e6
Mohnish Thallavajhula <m@mt.cx>
2017-10-09 14:59:07 -0700

    add specs for api/leads

--- spec/marketo_api/api/leads_spec.rb
+++ spec/marketo_api/api/leads_spec.rb
@@ -1,4 +1,26 @@
 require 'spec_helper'
 
 describe MarketoApi::API::Leads do
+  subject { Class.new.extend(MarketoApi::API::Leads) }
+
+  describe '#leads_describe' do
+    it 'calls the leads/describe.json endpoint' do
+      expect(subject).to receive(:api_get).with('leads/describe.json')
+      subject.leads_describe
+    end
+  end
+
+  describe '#leads_by_id' do
+    it 'calls the leads/1337.json endpoint' do
+      expect(subject).to receive(:api_get).with('leads/1337.json')
+      subject.leads_by_id(1337)
+    end
+  end
+
+  describe '#leads_by_filter_type' do
+    it 'calls the leads.json?filterType=foo&filterValues=one,two endpoint' do
+      expect(subject).to receive(:api_get).with('leads.json?filterType=foo&filterValues=one,two')
+      subject.leads_by_filter_type('foo', ['one', 'two'])
+    end
+  end
 end
