1f31484baac222644186db41602a7fabd8332fbd
Mohnish Thallavajhula <m@mt.cx>
2017-10-11 16:18:32 -0700

    add specs for concerns/base

--- spec/marketo_api/concerns/base_spec.rb
+++ spec/marketo_api/concerns/base_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 
 describe MarketoApi::Concerns::Base do
-  subject do
+  let(:klass) do
     class Foo
       include MarketoApi::Concerns::Base
     end
@@ -9,7 +9,35 @@ describe MarketoApi::Concerns::Base do
 
   describe '.new' do
     it 'raises when exception when the opts is not a hash' do
-      expect{ subject.new([]) }.to raise_error(ArgumentError, 'Please specify a hash of options')
+      expect{ klass.new([]) }.to raise_error(ArgumentError, 'Please specify a hash of options')
+    end
+  end
+
+  describe '#instance_url' do
+    context 'when an instance url is present' do
+      let(:obj) { klass.new({ instance_url: 'http://example.com' }) }
+
+      it 'returns the url' do
+        expect(obj).not_to receive(:authenticate!)
+        expect(obj.instance_url).to eql('http://example.com')
+      end
+    end
+
+    context 'when an instance url is missing' do
+      let(:obj) { klass.new({}) }
+
+      it 'calls authenticate!' do
+        expect(obj).to receive(:authenticate!)
+        obj.instance_url
+      end
+    end
+  end
+
+  describe '#inspect' do
+    let(:obj) { klass.new({}) }
+
+    it 'returns a string representation of the obj' do
+      expect(obj.inspect).to include('#<Foo @options=')
     end
   end
 end
