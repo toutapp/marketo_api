acc7ce9415dc17cff8f1ec08dbf835d8202e1ccb
Mohnish Thallavajhula <m@mt.cx>
2017-10-12 11:26:34 -0700

    add specs for concerns/connection

--- spec/marketo_api/concerns/connection_spec.rb
+++ spec/marketo_api/concerns/connection_spec.rb
@@ -1,49 +1,15 @@
 require 'spec_helper'
 
 describe MarketoApi::Concerns::Connection do
-  describe '.middleware' do
-    subject       { client.middleware }
-    let(:builder) { double('Faraday::Builder') }
+  subject { Class.new.extend(MarketoApi::Concerns::Connection) }
 
-    before do
-      client.stub_chain :connection, builder: builder
+  describe '#middleware' do
+    let(:connection_mock) { double('faraday') }
+
+    it 'creates a connection' do
+      expect(subject).to receive(:connection).and_return(connection_mock)
+      expect(connection_mock).to receive(:builder)
+      subject.middleware
     end
-
-    it { should eq builder }
-  end
-
-  describe "#connection_options" do
-    let(:options) { { ssl: { verify: false } } }
-    before { client.stub(options: options) }
-
-    it "picks up passed-in SSL options" do
-      expect(client.send(:connection_options)).to include(options)
-    end
-  end
-
-  describe 'private #connection' do
-    describe ":logger option" do
-      let(:options) { { adapter: Faraday.default_adapter } }
-
-      before(:each) do
-        client.stub(:authentication_middleware)
-        client.stub(:cache)
-        client.stub(options: options)
-        MarketoApi.stub(log?: true)
-      end
-
-      it "must always be used last before the Faraday Adapter" do
-        client.middleware.handlers.reverse.index(MarketoApi::Middleware::Logger).
-          should eq 1
-      end
-    end
-  end
-
-  describe '#adapter' do
-    before do
-      client.stub options: { adapter: :typhoeus }
-    end
-
-    its(:adapter) { should eq(:typhoeus) }
   end
 end
