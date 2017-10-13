1afc1e3e79b10634584f00457162f314313dc1be
Mohnish Thallavajhula <m@mt.cx>
2017-10-12 18:12:25 -0700

    remove unnecessary specs

--- spec/marketo_api/abstract_client_spec.rb
+++ spec/marketo_api/abstract_client_spec.rb
@@ -1,14 +1,5 @@
 require 'spec_helper'
 
 describe MarketoApi::AbstractClient do
-  subject { described_class }
 
-  it { should < MarketoApi::Concerns::Base }
-  it { should < MarketoApi::Concerns::Connection }
-  it { should < MarketoApi::Concerns::Authentication }
-  it { should < MarketoApi::Concerns::Caching }
-
-  it { should < MarketoApi::API::Base }
-  it { should < MarketoApi::API::Leads }
-  it { should < MarketoApi::API::Activities }
 end
--- spec/marketo_api/middleware/authorization_spec.rb
+++ spec/marketo_api/middleware/authorization_spec.rb
@@ -1,11 +1,5 @@
 require 'spec_helper'
 
 describe MarketoApi::Middleware::Authorization do
-  let(:options) { { oauth_token: 'token' } }
 
-  describe '.call' do
-    subject { lambda { middleware.call(env) } }
-
-    it { should change { env[:request_headers]['Authorization'] }.to eq 'Bearer token' }
-  end
 end
