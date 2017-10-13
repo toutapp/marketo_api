aaed5df14b60cd5a67baf546eaa51d5c6e1ba565
Mohnish Thallavajhula <m@mt.cx>
2017-10-12 17:55:40 -0700

    add specs for middleware/authentication/token

--- spec/marketo_api/middleware/authentication/token_spec.rb
+++ spec/marketo_api/middleware/authentication/token_spec.rb
@@ -1,27 +1,20 @@
 require 'spec_helper'
 
 describe MarketoApi::Middleware::Authentication::Token do
-  let(:options) do
-    {
-      instance_url: 'https://login.marketo.com',
-      client_id: 'client_id',
-      client_secret: 'client_secret',
-      adapter: :net_http }
+  subject do
+    MarketoApi::Middleware::Authentication::Token.new(double, double, {
+      client_id: 'client_id_x',
+      client_secret: 'client_secret_x'
+    })
   end
 
-  it_behaves_like 'authentication middleware' do
-    let(:success_request) do
-      stub_login_request(
-        body: "grant_type=client_credentials&" \
-                 "client_id=client_id&client_secret=client_secret"
-      ).to_return(status: 200, body: fixture(:auth_success_response))
-    end
-
-    let(:fail_request) do
-      stub_login_request(
-        body: "grant_type=client_credentials&" \
-                 "client_id=client_id&client_secret=client_secret"
-      ).to_return(status: 400, body: fixture(:refresh_error_response))
+  describe '#params' do
+    it 'returns the params' do
+      expect(subject.params).to eql({
+        grant_type: 'client_credentials',
+        client_id: 'client_id_x',
+        client_secret: 'client_secret_x'
+      })
     end
   end
 end
