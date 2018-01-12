177171d9b9c39fea8db86b4044642a770659bee8
tim-lee92 <tim-lee92@outlook.com>
2018-01-12 15:55:19 -0800

    Add inline documentation

--- lib/marketo_api/api/workspaces.rb
+++ lib/marketo_api/api/workspaces.rb
@@ -3,6 +3,20 @@ module MarketoApi
     module Workspaces
       include Base
 
+      # Public: Returns an array of workspaces for the current user.
+      # Example
+      # client.user_workspaces
+      # => [{
+      #       "id": 102,
+      #       "name": "A cool Marketo workspace",
+      #       "description": "This workspace is amazing!",
+      #       "globalViz": 0,
+      #       "status": "active",
+      #       "currencyInfo": nil,
+      #       "createdAt": "20180112T18:07:09.0t+0000",
+      #       "updatedAt": "20180112T18:07:09.0t+0000"
+      #    }]
+
       def user_workspaces
         get("userservice/lm/v1/workspaces/")
       end
